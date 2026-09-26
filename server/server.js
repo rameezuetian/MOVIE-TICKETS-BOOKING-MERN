555b73d014aa1ea64cc4f86d762e42b0c5989ef0
rameezuetian <muhammadrameez262@gmail.com>
2026-09-26 13:43:53 +0500

    update server.js

--- server/server.js
+++ server/server.js
@@ -2,10 +2,9 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
-
+import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,19 +12,26 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(clerkMiddleware())
-
+app.use(clerkMiddleware());
 
 // MongoDB
 connectDB();
 
-// Route
+// Routes
 app.get("/", (req, res) => {
-  res.send("Server is Live!");
+    res.send("Server is Live!");
 });
-app.use('/api/inngest' , serve({client:inngest , functions}))
+
+// Inngest
+app.use(
+    "/api/inngest",
+    serve({
+        client: inngest,
+        functions,
+    })
+);
 
 // Start server
 app.listen(port, () => {
-  console.log(`Server is running on localhost: ${port}`);
+    console.log(`Server is running on localhost: ${port}`);
 });
