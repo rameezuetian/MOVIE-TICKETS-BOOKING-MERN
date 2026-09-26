ab815741b990b42a349fad78e152033dbde226eb
rameezuetian <muhammadrameez262@gmail.com>
2026-09-26 13:16:33 +0500

    inngest setup

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +13,8 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware())
+
 
 // MongoDB
 connectDB();
@@ -17,6 +23,7 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("Server is Live!");
 });
+app.use('/api/inngest' , serve({client:inngest , functions}))
 
 // Start server
 app.listen(port, () => {
